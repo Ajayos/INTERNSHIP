6c81eeb8e4bf33fac6ba64a6c50a6e3726e50ab1
Ajay o s <ajayosakhub@gmail.com>
2023-05-08 09:28:51 +0530

    ADDED

--- html/arrayofobject.js
+++ html/arrayofobject.js
@@ -1,7 +1,7 @@
 // filepath: html/arrayofobject.js
 
 // array of objects 
-var marks = [ {name:'Ajay o s', age: 20}, {name:'akash', age:20}]
+var marks = [ {name:'Ajay o s', age: 20, place: "perumbavoor"}, {name:'akash', age:20, place: "perumbavoor"}, {name:'chem j', age:20, place: "perumbavoor"}]
 console.log(marks);
 
 // array of objects of index 0
