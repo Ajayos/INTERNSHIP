b65d1531e7b91789b0de7f306dc6429184b8df70
Ajay o s <ajayosakhub@gmail.com>
2023-05-08 09:25:51 +0530

    ADDED

---
+++ html/arrayofobject.js
@@ -0,0 +1,13 @@
+// filepath: html/arrayofobject.js
+
+// array of objects 
+var marks = [ {name:'Ajay o s', age: 20}, {name:'akash', age:20}]
+console.log(marks);
+
+// array of objects of index 0
+console.log(marks[0].name);
+console.log(marks[0].age);
+
+
+// using concatination
+console.log(`Name : ${marks[0].name}, Age : ${marks[0].age}`);
